493bb62c214ce75257ac92d75fde375bda3e9397
IthriKendi <98861518+IthriKendi@users.noreply.github.com>
2023-09-24 14:38:47 -0400

    Tloc function

--- tp1/src/main/java/ift3931/App.java
+++ tp1/src/main/java/ift3931/App.java
@@ -1,13 +1,48 @@
 package ift3931;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        System.out.println(tloc("tp1\\src\\test\\java\\ift3931\\AppTest.java"));
     }
+
+    public static long tloc(String stringPath) throws IOException{
+
+        long numLines = 0;
+        String line;
+        Boolean read = true;
+
+        BufferedReader br = new BufferedReader(new FileReader(stringPath));
+
+        while ((line = br.readLine()) != null) {
+            if (line.contains("//") || line.isBlank() || line.isEmpty()){
+                continue;
+            }
+            else if(line.contains("/*")) {
+                read = false;
+            }
+            else if(line.contains("*/")) {
+                read = true;
+                continue;
+            }
+
+            if (!read) continue; 
+
+            numLines++;
+        };
+
+        br.close();
+        
+        return numLines;
+    }
+
 }
